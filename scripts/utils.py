4b44f96229ee4069ed8e0ffe8134569766c978bd
syevers <32802098+syevers@users.noreply.github.com>
2024-03-18 22:47:15 -0700

    Added load_images to load multiple images at a time

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,4 +1,5 @@
 import pygame
+import os
 
 BASE_IMG_PATH = 'data/images/'
 
@@ -8,3 +9,12 @@ def load_image(path):
     img = pygame.image.load(BASE_IMG_PATH + path).convert()
     img.set_colorkey((0, 0, 0))
     return img
+
+
+# loading multiple images at once
+def load_images(path):
+    images = []
+    for img_name in sorted(os.listdir(BASE_IMG_PATH + path)):
+        images.append(load_image(path + '/' + img_name))
+    return images
+
